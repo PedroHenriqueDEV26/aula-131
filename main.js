0e5d7ff02992230518379d9456a42f590268d3e4
PedroHenriqueDEV26 <112581681+PedroHenriqueDEV26@users.noreply.github.com>
2024-07-12 15:36:25 -0300

    aula 134

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var img = ""
 var status = ""
-
+var objects = []
 
 function preload(){
     img = loadImage("setup.jpg")
@@ -9,15 +9,23 @@ function preload(){
 
 
 function setup(){
- canvas = createCanvas(500, 500)
+ canvas = createCanvas(600, 500)
  canvas.center()
+ video1 = createCapture(VIDEO)
+ video1.hide()
  objDetector = ml5.objectDetector("cocossd", modelLoaded)
+}
+
+function start(){
+    objDetector = ml5.objectDetector("cocossd", modelLoaded)
+    document.getElementById("status").innerHTML = "detectando objetos..."
+
 }
 
 function modelLoaded(){
     console.log("modelo carregado!")
     status = true;
-    objDetector.detect(img, gotResult);
+    objDetector.detect(video1, gotResult);
 }
 
 function gotResult(error, results){
@@ -25,19 +33,37 @@ function gotResult(error, results){
         console.log(error)
     }
     console.log(results)
+
+    objects = results;
+
+
+
 }
 
 function draw(){
-    image(img, 0,0, 500, 500) 
-    fill("red")
-    text("Cachorro", 100, 50)
-    noFill()
-    stroke("red")
-    rect(70, 60, 300, 300) 
+    image(video1, 0,0, width, height) 
+    
 
-    fill("blue")
-    text("Gato", 330, 95)
-    noFill()
-    stroke("blue")
-    rect(230, 100, 250, 350)
+
+    // fill("blue")
+    // text("Gato", 330, 95)
+    // noFill()
+    // stroke("blue")
+    // rect(230, 100, 250, 350)
+
+    if (status != "") {
+        objDetector.detect(video1, gotResult);
+        for (var j = 0; j < objects.length; j++) {
+            document.getElementById("status").innerHTML = "Objetos detectados"
+            document.getElementById("qtdObj").innerHTML = "Quantidade de objetos detectadas: "+ objects.length
+            fill("red")
+            percent = floor(objects[j].confidence*100)
+            text(objects[j].label+" "+percent+"%", objects[j].x, objects[j].y)
+
+            noFill()
+            stroke("red")
+            rect(objects[j].x, objects[j].y, objects[j].width, objects[j].height) 
+            
+        }
+    }
 }
